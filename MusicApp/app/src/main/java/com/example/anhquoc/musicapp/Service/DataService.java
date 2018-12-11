f63e2c15ce545c3d52426a1b2b6edf5ea59dc93a
CuongNguyen <cuongacoustic209@gmail.com>
2018-12-12 02:08:05 +0700

    Những file đã tác động:
    1)phần Adapter:
    BannerAdapter.java
    PlaylistAdapter.java
    
    2)phần Fragment:
    Fragment/Fragment_Banner.java
    Fragment/Fragment_Playlist.java
    Fragment/Fragment_Topic_Kind_Today.java
    
    3)phần Model:
    Quangcao.java
    Playlist.java
    Chude.java
    Theloai.java
    Theloaitrongngay.java
    
    4)phần Service:
    DataService
    
    5)phần drawable:
    custom_background_banner.xml
    
    6)phần layout:
    fragment_banner.xml
    fragment_playlist.xml
    fragment_topic_kind_today.xml
    line_banner.xml
    line_playlist.xml
    fragment_home.xml
    
    The end.

--- MusicApp/app/src/main/java/com/example/anhquoc/musicapp/Service/DataService.java
+++ MusicApp/app/src/main/java/com/example/anhquoc/musicapp/Service/DataService.java
@@ -1,5 +1,29 @@
 package com.example.anhquoc.musicapp.Service;
 
-public interface DataService {
+import com.example.anhquoc.musicapp.Model.Chude;
+import com.example.anhquoc.musicapp.Model.Playlist;
+import com.example.anhquoc.musicapp.Model.Quangcao;
+import com.example.anhquoc.musicapp.Model.Theloai;
+import com.example.anhquoc.musicapp.Model.Theloaitrongngay;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface DataService {
+    @GET("songbanner.php")
+    Call<List<Quangcao>> GetDataBanner();
+
+    @GET("playlistcurrentday.php")
+    Call<List<Playlist>> GetPlaylistCurrentDay();
+
+    @GET("chudevatheloaitrongngay.php")
+    Call<List<Chude>> Gettopic();
+
+    @GET("chudevatheloaitrongngay.php")
+    Call<List<Theloai>> Getkind();
+
+    @GET("chudevatheloaitrongngay.php")
+    Call<Theloaitrongngay> GetCategoryMusic();
 }
