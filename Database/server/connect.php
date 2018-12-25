f155521130db8b692fe2cb7a4d1cc62db519558c
mttd1026 <munnguyen77@gmail.com>
2018-12-25 23:05:48 +0700

    sa

---
+++ Database/server/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "musicapp";
+
+    $con = mysqli_connect($hostname,$username,$password,$databasename);
+    mysqli_query($con,"SET NAMES 'utf8'");
+?>
