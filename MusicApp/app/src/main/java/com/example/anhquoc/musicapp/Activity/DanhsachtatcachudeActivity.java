82942a4cf885245177bf61e8f05be77cee61e158
mttd1026 <munnguyen77@gmail.com>
2018-12-25 22:00:28 +0700

    asd

---
+++ MusicApp/app/src/main/java/com/example/anhquoc/musicapp/Activity/DanhsachtatcachudeActivity.java
@@ -0,0 +1,71 @@
+package com.example.anhquoc.musicapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.example.anhquoc.musicapp.Adapter.DanhsachtatcachudeAdapter;
+import com.example.anhquoc.musicapp.Model.Chude;
+import com.example.anhquoc.musicapp.R;
+import com.example.anhquoc.musicapp.Service.APIService;
+import com.example.anhquoc.musicapp.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DanhsachtatcachudeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerViewtatcachude;
+    Toolbar toolbartatcachude;
+    DanhsachtatcachudeAdapter danhsachtatcachudeAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danhsachtatcachude);
+        init();
+        GetData();
+
+    }
+
+    private void GetData() {
+        DataService dataService = APIService.getService();
+        Call<List<Chude>> callback = dataService.GetAllChuDe();
+        callback.enqueue(new Callback<List<Chude>>() {
+            @Override
+            public void onResponse(Call<List<Chude>> call, Response<List<Chude>> response) {
+                ArrayList<Chude> mangchude = (ArrayList<Chude>) response.body();
+                danhsachtatcachudeAdapter = new DanhsachtatcachudeAdapter(DanhsachtatcachudeActivity.this,mangchude);
+                recyclerViewtatcachude.setLayoutManager(new GridLayoutManager(DanhsachtatcachudeActivity.this,1));
+                recyclerViewtatcachude.setAdapter(danhsachtatcachudeAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Chude>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void init() {
+        recyclerViewtatcachude = findViewById(R.id.recyclerviewallchude);
+        toolbartatcachude = findViewById(R.id.toolbarallchude);
+        setSupportActionBar(toolbartatcachude);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbartatcachude.setTitleTextColor(getResources().getColor(R.color.mautim));
+        getSupportActionBar().setTitle("Tất Cả Chủ Đề");
+        toolbartatcachude.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ MusicApp/app/src/main/java/com/example/anhquoc/musicapp/Activity/DanhsachtheloaitheochudeActivity.java
@@ -0,0 +1,80 @@
+package com.example.anhquoc.musicapp.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+import com.example.anhquoc.musicapp.Adapter.DanhsachtheloaitheochudeAdapter;
+import com.example.anhquoc.musicapp.Model.Chude;
+import com.example.anhquoc.musicapp.Model.Theloai;
+import com.example.anhquoc.musicapp.R;
+import com.example.anhquoc.musicapp.Service.APIService;
+import com.example.anhquoc.musicapp.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DanhsachtheloaitheochudeActivity extends AppCompatActivity {
+    Chude chude;
+    RecyclerView recyclerViewtheloaitheochude;
+    Toolbar toolbartheloaitheochude;
+    DanhsachtheloaitheochudeAdapter danhsachtheloaitheochudeAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danhsachtheloaitheochude);
+        GetIntent();
+        init();
+        GetData();
+    }
+
+    private void GetData() {
+        DataService dataService = APIService.getService();
+        Call<List<Theloai>> callback = dataService.GetTheloaitheochude(chude.getIdChuDe());
+        callback.enqueue(new Callback<List<Theloai>>() {
+            @Override
+            public void onResponse(Call<List<Theloai>> call, Response<List<Theloai>> response) {
+                ArrayList<Theloai> mangtheloai = (ArrayList<Theloai>) response.body();
+                danhsachtheloaitheochudeAdapter = new DanhsachtheloaitheochudeAdapter(DanhsachtheloaitheochudeActivity.this,mangtheloai);
+                recyclerViewtheloaitheochude.setLayoutManager(new GridLayoutManager(DanhsachtheloaitheochudeActivity.this,2));
+                recyclerViewtheloaitheochude.setAdapter(danhsachtheloaitheochudeAdapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Theloai>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void init() {
+        recyclerViewtheloaitheochude = findViewById(R.id.recyclerviewtheloaitheochude);
+        toolbartheloaitheochude = findViewById(R.id.toolbartheloaitheochude);
+        setSupportActionBar(toolbartheloaitheochude);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(chude.getTenChuDe());
+        toolbartheloaitheochude.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void GetIntent() {
+        Intent intent = getIntent();
+        if (intent.hasExtra("chude")){
+            chude = (Chude) intent.getSerializableExtra("chude");
+        }
+    }
+}
